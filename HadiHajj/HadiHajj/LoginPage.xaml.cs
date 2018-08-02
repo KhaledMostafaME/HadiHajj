5c8a5fcce773245f25e41a6e907f62050edb9200
Khaled Mostafa <KhaledMostafaME@outlook.com>
2018-08-02 17:28:57 +0300

    Add project files.

---
+++ HadiHajj/HadiHajj/LoginPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HadiHajj
+{
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void btn_login_ClickedAsync(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+
+        }
+
+        private void btn_signup_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Signup());
+
+        }
+    }
+}
